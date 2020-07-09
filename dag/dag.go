1e1a4e003169a91455d13eb07ee003b617bdb480
David Gamba <davidgamba@gmail.com>
2020-07-08 21:35:07 -0600

    Add code to execute DAG Tasks

--- dag/dag.go
+++ dag/dag.go
@@ -1,52 +1,117 @@
 package dag
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/DavidGamba/go-getoptions"
 )
 
-type ID string
+var Logger = log.New(os.Stderr, "", log.LstdFlags)
 
-type Task struct {
-	ID ID
-	Fn getoptions.CommandFn
-}
+type (
+	ID string
 
-func NewTask(id ID, fn getoptions.CommandFn) *Task {
-	return &Task{
-		ID: id,
-		Fn: fn,
+	Task struct {
+		ID ID
+		Fn getoptions.CommandFn
 	}
-}
 
-type Vertex struct {
-	ID       ID
-	Task     *Task
-	Children []*Vertex
-}
+	Vertex struct {
+		ID       ID
+		Task     *Task
+		Children []*Vertex
+		Parents  []*Vertex
+		status   runStatus
+	}
 
-type Graph struct {
-	Vertices map[ID]*Vertex
-}
+	Graph struct {
+		TickerDuration time.Duration
+		Vertices       map[ID]*Vertex
+		dotDiagram     string
+	}
+
+	runStatus int
+
+	visitStatus int
+)
+
+const (
+	runPending runStatus = iota
+	runInProgress
+	runDone
+)
+
+const (
+	// Vertex hasn't been visited
+	unvisited visitStatus = iota
+	// Vertex has been seen but not traversed
+	visited
+	// Vertex children have been traversed
+	traversed
+)
 
 var ErrorTaskDuplicate = fmt.Errorf("graph already contains task definition")
 var ErrorTaskNotFound = fmt.Errorf("task not found in graph")
 var ErrorTaskDependencyDuplicate = fmt.Errorf("task dependency already defined")
 var ErrorGraphHasCycle = fmt.Errorf("Graph has a cycle")
 
-func NewGraph() *Graph {
-	return &Graph{make(map[ID]*Vertex, 0)}
+// NewTask - Allows creating a reusable Task that can be passed to multiple graphs.
+func NewTask(id string, fn getoptions.CommandFn) *Task {
+	return &Task{
+		ID: ID(id),
+		Fn: fn,
+	}
 }
 
+// NewGraph - Create a graph that allows running Tasks in parallel.
+func NewGraph() *Graph {
+	return &Graph{
+		TickerDuration: 1 * time.Millisecond,
+		Vertices:       make(map[ID]*Vertex, 0),
+	}
+}
+
+// String - Returns a dot diagram of the graph.
+func (g *Graph) String() string {
+	dotDiagramHeader := `
+digraph G {
+	rankdir = TB;
+`
+
+	dotDiagramFooter := `}`
+
+	return dotDiagramHeader + g.dotDiagram + dotDiagramFooter
+}
+
+// CreateTask - Helper to avoid having to call NewTask and then AddTask if there is no need for reusable Tasks.
+func (g *Graph) CreateTask(id string, fn getoptions.CommandFn) error {
+	t := &Task{
+		ID: ID(id),
+		Fn: fn,
+	}
+	return g.AddTask(t)
+}
+
+// AddTask - Add Task to graph.
 func (g *Graph) AddTask(t *Task) error {
 	if _, ok := g.Vertices[t.ID]; ok {
 		return fmt.Errorf("%w: %s", ErrorTaskDuplicate, t.ID)
 	}
-	g.Vertices[t.ID] = &Vertex{t.ID, t, make([]*Vertex, 0)}
+	g.Vertices[t.ID] = &Vertex{
+		ID:       t.ID,
+		Task:     t,
+		Children: make([]*Vertex, 0),
+		Parents:  make([]*Vertex, 0),
+		status:   runPending,
+	}
 	return nil
 }
 
+// TaskDependensOn - Allows defining the edges of the Task graph.
 func (g *Graph) TaskDependensOn(t ID, tDependencies ...ID) error {
 	task, ok := g.Vertices[t]
 	if !ok {
@@ -62,28 +127,134 @@ func (g *Graph) TaskDependensOn(t ID, tDependencies ...ID) error {
 				return fmt.Errorf("%w: %s -> %s", ErrorTaskDependencyDuplicate, task.ID, dependency.ID)
 			}
 		}
+		g.dotDiagram += fmt.Sprintf("\t\"%s\" -> \"%s\";\n", t, tDependency)
 		task.Children = append(task.Children, dependency)
+		dependency.Parents = append(dependency.Parents, task)
 	}
 	return nil
 }
 
-// GetStartTasks - finds leaf nodes.
-func (g *Graph) GetStartVertex() ([]*Vertex, error) {
-	s := []*Vertex{}
-	for _, vertex := range g.Vertices {
-		if len(vertex.Children) == 0 {
-			s = append(s, vertex)
+// Run - Execute the graph tasks in parallel where possible.
+// It checks for tasks updates every 1 Millisecond by default.
+// Modify using the graph.TickerDuration
+func (g *Graph) Run(ctx context.Context, opt *getoptions.GetOpt, args []string) error {
+	// Check for cycles
+	_, err := g.DephFirstSort()
+	if err != nil {
+		return err
+	}
+
+	done := make(chan ID)
+	count := 0
+LOOP:
+	for {
+		select {
+		case id := <-done:
+			count++
+			Logger.Printf("count %d\n", count)
+			g.Vertices[id].status = runDone
+			if count >= len(g.Vertices) {
+				break LOOP
+			}
+		default:
+			v, ok := g.getNextVertex()
+			if !ok {
+				time.Sleep(g.TickerDuration)
+				continue
+			}
+			v.status = runInProgress
+			Logger.Printf("Running Task %s\n", v.ID)
+			go func(done chan ID, v *Vertex) {
+				start := time.Now()
+				v.Task.Fn(ctx, opt, args)
+				Logger.Printf("Completed Task %s in %s\n", v.ID, durationStr(time.Since(start)))
+				done <- v.ID
+			}(done, v)
 		}
 	}
-	return s, nil
+	return nil
 }
 
-// TODO: Add CheckCycle
+func durationStr(d time.Duration) string {
+	d = d.Round(time.Second)
+	m := d / time.Minute
+	d -= m * time.Minute
+	s := d / time.Second
+	return fmt.Sprintf("%02dm:%02ds", m, s)
+}
 
-func GetVertexIDs(list []*Vertex) []ID {
-	ids := []ID{}
-	for _, v := range list {
-		ids = append(ids, v.ID)
+// getNextVertex - Walks the graph to get the next Vertex that is pending and without pending dependencies.
+func (g *Graph) getNextVertex() (*Vertex, bool) {
+	for _, vertex := range g.Vertices {
+		if vertex.status != runPending {
+			continue
+		}
+		if len(vertex.Children) == 0 {
+			return vertex, true
+		}
+		childPending := false
+		for _, child := range vertex.Children {
+			if child.status == runPending {
+				childPending = true
+			}
+			if child.status == runInProgress {
+				childPending = true
+			}
+		}
+		if !childPending {
+			return vertex, true
+		}
 	}
-	return ids
+	return nil, false
+}
+
+// DephFirstSort - Returns a sorted list with the Vertices
+// https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+// It returns ErrorGraphHasCycle is there are cycles.
+func (g *Graph) DephFirstSort() ([]*Vertex, error) {
+	var err error
+
+	sorted := []*Vertex{}
+	status := map[ID]visitStatus{}
+
+	for _, vertex := range g.Vertices {
+		status[vertex.ID] = unvisited
+	}
+
+	for _, v := range g.Vertices {
+		if status[v.ID] != unvisited {
+			continue
+		}
+		err = visit(&sorted, &status, v)
+		if err != nil {
+			return sorted, err
+		}
+	}
+	fmt.Printf("sorted: %v\n", sorted)
+	for _, e := range sorted {
+		fmt.Printf(" %s ", e.ID)
+	}
+	fmt.Println()
+	return sorted, nil
+}
+
+func visit(sorted *[]*Vertex, status *map[ID]visitStatus, v *Vertex) error {
+	Logger.Printf("visiting %s\n", v.ID)
+	if (*status)[v.ID] == traversed {
+		return nil
+	}
+	if (*status)[v.ID] == visited {
+		return fmt.Errorf("%w: %s", ErrorGraphHasCycle, v.ID)
+	}
+	(*status)[v.ID] = visited
+
+	for _, child := range v.Children {
+		err := visit(sorted, status, child)
+		if err != nil {
+			return err
+		}
+	}
+	(*status)[v.ID] = traversed
+	*sorted = append(*sorted, v)
+	return nil
 }
--- dag/dag_test.go
+++ dag/dag_test.go
@@ -3,41 +3,47 @@ package dag
 import (
 	"context"
 	"errors"
-	"reflect"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/DavidGamba/go-getoptions"
 )
 
 func TestDag(t *testing.T) {
 	var err error
+
+	sm := sync.Mutex{}
 	results := []int{}
 	g := NewGraph()
-	addTask := func(t *Task) {
+	addTask := func(id string, fn getoptions.CommandFn) {
 		if err != nil {
 			return
 		}
-		err = g.AddTask(t)
+		err = g.CreateTask(id, fn)
 	}
-	generateTask := func(n int) getoptions.CommandFn {
+	generateFn := func(n int) getoptions.CommandFn {
 		return func(ctx context.Context, opt *getoptions.GetOpt, args []string) error {
+			sm.Lock()
+			time.Sleep(1 * time.Millisecond)
 			results = append(results, n)
+			sm.Unlock()
 			return nil
 		}
 	}
-	addTask(NewTask("t1", generateTask(1)))
-	addTask(NewTask("t2", generateTask(2)))
-	addTask(NewTask("t3", generateTask(3)))
-	addTask(NewTask("t4", generateTask(4)))
-	addTask(NewTask("t5", generateTask(5)))
-	addTask(NewTask("t6", generateTask(6)))
-	addTask(NewTask("t7", generateTask(7)))
-	addTask(NewTask("t8", generateTask(8)))
+	addTask("t1", generateFn(1))
+	addTask("t2", generateFn(2))
+	addTask("t3", generateFn(3))
+	addTask("t4", generateFn(4))
+	addTask("t5", generateFn(5))
+	addTask("t6", generateFn(6))
+	addTask("t7", generateFn(7))
+	addTask("t8", generateFn(8))
 	if err != nil {
 		t.Errorf("Unexpected error: %s\n", err)
 	}
 
-	err = g.AddTask(NewTask("t3", generateTask(3)))
+	err = g.AddTask(NewTask("t3", generateFn(3)))
 	if err == nil {
 		t.Errorf("Expected error none triggered\n")
 	}
@@ -80,12 +86,78 @@ func TestDag(t *testing.T) {
 		t.Errorf("Wrong error: %s\n", err)
 	}
 
-	start, err := g.GetStartVertex()
+	_, err = g.DephFirstSort()
 	if err != nil {
 		t.Errorf("Unexpected error: %s\n", err)
 	}
-	if len(start) != 2 || (!reflect.DeepEqual(GetVertexIDs(start), []ID{"t5", "t8"}) &&
-		!reflect.DeepEqual(GetVertexIDs(start), []ID{"t8", "t5"})) {
-		t.Errorf("Wrong start list: %v\n", GetVertexIDs(start))
+
+	err = g.Run(nil, nil, nil)
+	if err != nil {
+		t.Errorf("Unexpected error: %s\n", err)
+	}
+	for i, e := range results {
+		switch i {
+		case 0, 1:
+			if e != 5 && e != 8 {
+				t.Errorf("Wrong list: %v\n", results)
+			}
+		case 2, 3:
+			if e != 4 && e != 7 {
+				t.Errorf("Wrong list: %v\n", results)
+			}
+		case 4, 5:
+			if e != 2 && e != 3 {
+				t.Errorf("Wrong list: %v\n", results)
+			}
+		case 6, 7:
+			if e != 1 && e != 6 {
+				t.Errorf("Wrong list: %v\n", results)
+			}
+		}
+	}
+	expectedDiagram := `
+digraph G {
+	rankdir = TB;
+	"t1" -> "t2";
+	"t1" -> "t3";
+	"t2" -> "t4";
+	"t3" -> "t4";
+	"t4" -> "t5";
+	"t6" -> "t2";
+	"t6" -> "t8";
+	"t7" -> "t5";
+}`
+	if g.String() != expectedDiagram {
+		t.Errorf("Wrong output: '%s'\nexpected: '%s'\n", g.String(), expectedDiagram)
+	}
+}
+
+func TestCycle(t *testing.T) {
+	var err error
+	g := NewGraph()
+	addTask := func(id string, fn getoptions.CommandFn) {
+		if err != nil {
+			return
+		}
+		err = g.CreateTask(id, fn)
+	}
+	generateFn := func(n int) getoptions.CommandFn {
+		return func(ctx context.Context, opt *getoptions.GetOpt, args []string) error {
+			return nil
+		}
+	}
+	addTask("t1", generateFn(1))
+	addTask("t2", generateFn(2))
+
+	g.TaskDependensOn("t1", "t2")
+	g.TaskDependensOn("t2", "t1")
+	_, err = g.DephFirstSort()
+	if err == nil || !errors.Is(err, ErrorGraphHasCycle) {
+		t.Errorf("Wrong error: %s\n", err)
+	}
+
+	err = g.Run(nil, nil, nil)
+	if err == nil || !errors.Is(err, ErrorGraphHasCycle) {
+		t.Errorf("Wrong error: %s\n", err)
 	}
 }
