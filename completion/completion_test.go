f9881e7f32b8445b3ae5a3e98b80905da8dab937
David Gamba <davidgamba@gmail.com>
2020-07-18 01:24:41 -0600

    Completion: arguments to options using = don't break completion chain

--- completion/completion_test.go
+++ completion/completion_test.go
@@ -27,7 +27,7 @@ func TestGetChildNames(t *testing.T) {
 
 	// Tree setup
 	rootNode := NewNode("executable", Root, nil)
-	rootNode.AddChild(NewNode("options", OptionsNode, []string{"--version", "--help", "-v", "-h"}))
+	rootNode.AddChild(NewNode("options", OptionsNode, []string{"--version", "--help", "--profile", "-v", "-h", "-p"}))
 
 	logNode := NewNode("log", StringNode, nil)
 	rootNode.AddChild(logNode)
@@ -56,7 +56,7 @@ func TestGetChildNames(t *testing.T) {
 		{"get commands", rootNode, "", []string{"log", "logger", "show"}},
 		{"get commands", rootNode, "log", []string{"log", "logger"}},
 		{"get commands", rootNode, "show", []string{"show"}},
-		{"get options", rootNode, "-", []string{"-h", "--help", "-v", "--version"}},
+		{"get options", rootNode, "-", []string{"-h", "--help", "-p", "--profile", "-v", "--version"}},
 		{"get options", rootNode, "-h", []string{"-h"}},
 		{"get commands", rootNode.GetChildByName("x"), "", []string{}},
 		{"filter out hidden files", rootNode.GetChildByName("log"), "", []string{"sublog", "aFile1", "aFile2", "bDir1/", "bDir2/", "cFile1", "cFile2"}},
@@ -94,19 +94,30 @@ func TestGetChildNames(t *testing.T) {
 		{"nil", rootNode, "", []string{}},
 		{"top level", rootNode, "./executable ", []string{"log", "logger", "show"}},
 		{"top level", rootNode, "./executable  ", []string{"log", "logger", "show"}},
+		{"top level", rootNode, "./executable   ", []string{"log", "logger", "show"}},
 		{"top level", rootNode, "./executable l", []string{"log", "logger"}},
 		{"top level", rootNode, "./executable  l", []string{"log", "logger"}},
+		{"top level", rootNode, "./executable   l", []string{"log", "logger"}},
 		{"top level", rootNode, "./executable lo", []string{"log", "logger"}},
 		{"top level", rootNode, "./executable log", []string{"log", "logger"}},
 		{"top level", rootNode, "./executable  log", []string{"log", "logger"}},
 		{"top level", rootNode, "./executable sh", []string{"show"}},
-		{"options", rootNode, "./executable -", []string{"-h", "--help", "-v", "--version"}},
+		{"options", rootNode, "./executable -", []string{"-h", "--help", "-p", "--profile", "-v", "--version"}},
 		{"options", rootNode, "./executable -h", []string{"-h"}},
 		{"options", rootNode, "./executable -h ", []string{"log", "logger", "show"}},
 		{"options", rootNode, "./executable  -h  l", []string{"log", "logger"}},
+		{"options", rootNode, "./executable  --help  l", []string{"log", "logger"}},
+		{"options", rootNode, "./executable  --profile  l", []string{"log", "logger"}},
+		{"options", rootNode, "./executable  --profile=dev  l", []string{"log", "logger"}},
+		{"options", rootNode, "./executable  --profile", []string{"--profile"}},
+		{"options", rootNode, "./executable  --profile=dev", []string{"--profile=dev"}},
+		// TODO:
+		// {"options", rootNode, "./executable  --profile dev  l", []string{"log", "logger"}},
 		{"command", rootNode, "./executable log ", []string{"sublog", "aFile1", "aFile2", "bDir1/", "bDir2/", "cFile1", "cFile2"}},
 		{"command", rootNode, "./executable log bDir1/f", []string{"bDir1/file"}},
 		{"command", rootNode, "./executable log bDir1/file ", []string{"sublog", "aFile1", "aFile2", "bDir1/", "bDir2/", "cFile1", "cFile2"}},
+		{"command", rootNode, "./executable log bDir1/file -", []string{"--help"}},
+		{"command", rootNode, "./executable   log   bDir1/file  -", []string{"--help"}},
 		{"command", rootNode, "./executable logger ../.a", []string{"../.aFile2"}},
 		{"command", rootNode, "./executable logger ../.aFile2", []string{"../.aFile2"}},
 		{"command", rootNode, "./executable show", []string{"abcd1234", "bbcd/1234", "..hola", "--hola"}},
