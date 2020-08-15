cb3d9ad28c81ceb0bdcaaa9eb31828f87fe336d6
David Gamba <davidgamba@gmail.com>
2020-08-14 23:49:12 -0600

    Update README with examples information

--- examples/myscript/sourceme.bash
+++ examples/myscript/sourceme.bash
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Remove existing entries to ensure the right one is loaded
+# This is not required when the completion one liner is loaded in your bashrc.
+complete -r myscript 2>/dev/null
+
 complete -o default -C "$PWD/myscript" myscript
