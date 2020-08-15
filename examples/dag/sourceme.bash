980b7bff26983ad42fba2045ca3cfae68904933c
David Gamba <davidgamba@gmail.com>
2020-08-15 00:04:36 -0600

    Update README dag information

--- examples/dag/sourceme.bash
+++ examples/dag/sourceme.bash
@@ -2,6 +2,6 @@
 
 # Remove existing entries to ensure the right one is loaded
 # This is not required when the completion one liner is loaded in your bashrc.
-complete -r ./mygit 2>/dev/null
+complete -r ./dag 2>/dev/null
 
-complete -o default -C "$PWD/mygit" mygit
+complete -o default -C "$PWD/dag" dag
--- examples/myscript/sourceme.bash
+++ examples/myscript/sourceme.bash
@@ -2,6 +2,6 @@
 
 # Remove existing entries to ensure the right one is loaded
 # This is not required when the completion one liner is loaded in your bashrc.
-complete -r myscript 2>/dev/null
+complete -r ./myscript 2>/dev/null
 
 complete -o default -C "$PWD/myscript" myscript
