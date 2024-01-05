b7ada72ecac69db22a3981b0af9b91bf0bf60177
David Gamba <davidgamba@gmail.com>
2024-01-04 23:52:43 -0700

    dag: extend parallel test time to fix flaky test

--- dag/dag_test.go
+++ dag/dag_test.go
@@ -754,7 +754,7 @@ func TestMaxParallel(t *testing.T) {
 			// routines to reach it then release the lock.
 			workMutex.Lock()
 			go func() {
-				time.Sleep(30 * time.Millisecond)
+				time.Sleep(50 * time.Millisecond)
 				workMutex.Unlock()
 			}()
 
