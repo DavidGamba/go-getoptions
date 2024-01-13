2bfd31b7a36823abd34b0a286ea09e174860a1fc
David Gamba <davidgamba@gmail.com>
2024-01-12 21:48:28 -0700

    bake: extract build into separate file and refactor

--- bake/main.go
+++ bake/main.go
@@ -19,9 +19,6 @@ import (
 	"unicode"
 	"unsafe"
 
-	"github.com/DavidGamba/dgtools/buildutils"
-	"github.com/DavidGamba/dgtools/fsmodtime"
-	"github.com/DavidGamba/dgtools/run"
 	"github.com/DavidGamba/go-getoptions"
 	"github.com/DavidGamba/go-getoptions/dag"
 	"golang.org/x/tools/go/packages"
@@ -230,79 +227,6 @@ func camelToKebab(camel string) string {
 		buffer.WriteRune(unicode.ToLower(ch))
 	}
 	return buffer.String()
-}
-
-func findBakeFiles(ctx context.Context) (string, error) {
-	wd, err := os.Getwd()
-	if err != nil {
-		return "", fmt.Errorf("failed to get working directory: %w", err)
-	}
-
-	// First case, we are withing the bake folder
-	base := filepath.Base(wd)
-	if base == "bake" {
-		err := build(".")
-		if err != nil {
-			return "", fmt.Errorf("failed to build: %w", err)
-		}
-		return "./bake.so", nil
-	}
-
-	// Second case, bake folder lives in CWD
-	dir := filepath.Join(wd, "bake")
-	if fi, err := os.Stat(dir); err == nil && fi.Mode().IsDir() {
-		err := build(dir)
-		if err != nil {
-			return "", fmt.Errorf("failed to build: %w", err)
-		}
-		return filepath.Join(dir, "bake.so"), nil
-	}
-
-	// Third case, bake folder lives in module root
-	modRoot, err := buildutils.GoModDir()
-	if err != nil {
-		return "", fmt.Errorf("failed to get go project root: %w", err)
-	}
-	dir = filepath.Join(modRoot, "bake")
-	if fi, err := os.Stat(dir); err == nil && fi.Mode().IsDir() {
-		err := build(dir)
-		if err != nil {
-			return "", fmt.Errorf("failed to build: %w", err)
-		}
-		return filepath.Join(dir, "bake.so"), nil
-	}
-
-	// Fourth case, bake folder lives in root of repo
-	root, err := buildutils.GitRepoRoot()
-	if err != nil {
-		return "", fmt.Errorf("failed to get git repo root: %w", err)
-	}
-	dir = filepath.Join(root, "bake")
-	if fi, err := os.Stat(dir); err == nil && fi.Mode().IsDir() {
-		err := build(dir)
-		if err != nil {
-			return "", fmt.Errorf("failed to build: %w", err)
-		}
-		return filepath.Join(dir, "bake.so"), nil
-	}
-
-	return "", fmt.Errorf("bake directory not found")
-}
-
-func build(dir string) error {
-	files, modified, err := fsmodtime.Target(os.DirFS(dir),
-		[]string{"bake.so"},
-		[]string{"*.go", "go.mod", "go.sum"})
-	if err != nil {
-		return err
-	}
-	if modified {
-		Logger.Printf("Found modifications on %v, rebuilding...\n", files)
-		// Debug flags
-		// return run.CMD("go", "build", "-buildmode=plugin", "-o=bake.so", "-trimpath", "-gcflags", "all=-N -l").Dir(dir).Log().Run()
-		return run.CMD("go", "build", "-buildmode=plugin", "-o=bake.so").Dir(dir).Log().Run()
-	}
-	return nil
 }
 
 func ListSymbolsRun(bakefile string) getoptions.CommandFn {
